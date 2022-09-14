9306829aad5cbe9ca64bf42c8bfae83ffd0da97d
zhangheng <zh@qq.com>
2022-09-14 09:44:15 +0800

    second commit

---
+++ src/main/java/com/zh/GitTest.java
@@ -0,0 +1,13 @@
+package com.zh;
+
+/**
+ * @author 张恒
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
